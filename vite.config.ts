59e5a8ca03981684c09ea35f63f11b7168d4e633
Prabhas Kurapati <prabhask@berkeley.edu>
2026-03-01 15:36:26 +0000

    [BUGFIX] content cache error fix

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,8 @@ export default defineConfig({
 				]
 			},
 			workbox: {
-				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}'],
+				navigateFallback: null
 			}
 		})
 	]
